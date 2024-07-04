e9e88aac23db1027b376a17316953c3f6d1bf606
Gregor Morrill <git@gregorlove.com>
2024-07-04 12:36:24 -0700

    Update scoped dependencies

--- config/php-scoper/scoper.inc.php
+++ config/php-scoper/scoper.inc.php
@@ -8,7 +8,7 @@ use Isolated\Symfony\Component\Finder\Finder;
 
 return [
     'prefix'  => 'IndieAuth\\Libs',
-
+    'output-dir' => 'scoped-libs',
     'finders' => [
         Finder::create()->files()->in( 'vendor/mf2/mf2' )->name( [ '*.php', 'LICENSE', 'composer.json' ] ),
         Finder::create()->files()->in( 'vendor/barnabywalters/mf-cleaner' )->name( [ '*.php', 'LICENSE', 'composer.json' ] ),
